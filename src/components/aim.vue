ac5239f8e4471194ab4a722ecdec85c226e99625
vinayaksh42 <vinayaksh42@gmail.com>
2020-05-22 14:56:02 +0530

    Aim para

---
+++ src/components/aim.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="row">
+        <div class="col-6 aim">
+            <h2>Aim of our organization</h2>
+            <p>our website provides a comprehensive list of open source events and hackathons that our happening around the world.
+                They are listed down in a proper month wise timeline so the user does not face difficulty while searching through the open source events.
+                Most of the times students want to participate in the events but they dont know the dates or the event details.
+                This repo contains links of the events and when are the events going to take place.</p>
+        </div>
+        <div class="col-6">
+            <img src="../assets/giphy.gif" width="450" height="240"/>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style scoped>
+    .aim{
+        font-family: 'Ubuntu', sans-serif;
+        text-align: justify;
+    }
+</style>
