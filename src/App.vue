1c8929000d562c541246aa4b193f5d744f8e3d47
vinayaksh42 <vinayaksh42@gmail.com>
2020-05-23 18:34:09 +0530

    completed project component

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,16 @@
         <div class="container">
             <appheader></appheader>
         </div>
-        <hr class="container">
+        <hr>
         <div class="container">
             <appaim></appaim>
+            <hr>
+        </div>
+        <div class="container">
+            <h3>Completed projects</h3>
+            <br>
+            <cp :details="completedprojects"></cp>
+            <br><br>
         </div>
         <hr class="container">
         <div class="container">
@@ -18,16 +25,29 @@
 import header from "./components/header";
 import footer from "./components/footer";
 import aim from "./components/aim";
+import gridView from "./components/gridView";
 
 export default {
+    data: function(){
+        return {
+            completedprojects: [
+                {title: 'chatbot', body: 'This chatbot helps the police in automating response from citizens'},
+                {title: 'sample', body: 'sample text for holding place'}
+            ]
+        };
+    },
     components: {
         appheader: header,
         appfooter: footer,
-        appaim: aim
+        appaim: aim,
+        cp: gridView
     }
 }
 </script>
 
-<style>
-
+<style scoped>
+h3{
+    color: #f8ba26;
+    font-family: 'Ubuntu', sans-serif;
+}
 </style>
