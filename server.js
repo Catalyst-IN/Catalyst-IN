463e1e790bdeebc21bffaba23cdc2720e19ec9f9
vinayaksh42 <vinayaksh42@gmail.com>
2020-05-26 14:16:26 +0530

    initial

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+const serveStatic = require('serve-static');
+
+app = express();
+
+app.use('/', serveStatic(path.join(__dirname, '/dist')))
+
+const port = process.env.PORT || 8080;
+app.listen(port);
+
+console.log('server started '+ port);
