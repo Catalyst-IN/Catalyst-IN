21ff22f9805f0e1a000d9605764790b662522627
Vinayak Sharma <54861487+vinayaksh42@users.noreply.github.com>
2020-05-30 12:32:19 +0530

    content

--- src/whatwedo.vue
+++ src/whatwedo.vue
@@ -1,6 +1,18 @@
 <template>
     <div class="container">
-        <h3>What we do</h3>
+        <br><br><br>
+        <h1><i class="fas fa-infinity"></i> What we do</h1>
+        <br>
+        <div class="row">
+            <div class="col-sm">
+            </div>
+            <div class="col-sm">
+                <p>We bring amazing people together to create, develop, design and make things with Open Technologies - hardware and software - and share our work for the benefit of all.</p>
+            </div>
+            <div class="col-sm">
+            </div>
+        </div>
+        <br><br><br>
     </div>
 </template>
 
@@ -11,5 +23,14 @@
 </script>
 
 <style scoped>
+    h1{
+        text-align: center;
+        color: #f8ba26;
 
+    }
+    p{
+        font-family: "Ubuntu", sans-serif;
+        font-size: 24px;
+        text-align: justify;
+    }
 </style>
--- src/whoweare.vue
+++ src/whoweare.vue
@@ -1,6 +1,20 @@
 <template>
     <div class="container">
-        <h3>Who we are</h3>
+        <br><br><br>
+        <h1><i class="fas fa-users" ></i> Who we are</h1>
+        <br>
+        <div class="row">
+            <div class="col-sm">
+
+            </div>
+            <div class="col-sm">
+                <p>We are an organization of people from all over the world. We want to see progress and improvements of people's lives in Asia and everywhere. We are developers, designers, event organizers and contributors who want to learn and share ideas, create and make digital software tools and hardware, that benefit people around the world.</p>
+            </div>
+            <div class="col-sm">
+
+            </div>
+        </div>
+        <br><br><br>
     </div>
 </template>
 
@@ -11,5 +25,14 @@
 </script>
 
 <style scoped>
+    h1{
+        text-align: center;
+        color: #f8ba26;
 
+    }
+p{
+    font-family: "Ubuntu", sans-serif;
+    font-size: 24px;
+    text-align: justify;
+}
 </style>
