9661a4fd43088353f034532a75ece1da502b911c
Vinayak Sharma <54861487+vinayaksh42@users.noreply.github.com>
2020-06-13 14:43:15 +0530

    mobile responsive

--- src/components/activeProjects.vue
+++ src/components/activeProjects.vue
@@ -1,19 +1,20 @@
 <template>
-  <div class="col-sm-6 col-md-4 col-lg-3">
-    <div id="root">
-      <div class="box">
-        <div class="content">
-          <slot name="image"></slot>
-          <slot name="title" class="title"></slot>
-          <slot name="subtitle"></slot>
-          <hr />
-          <slot name="body" class="content"></slot>
-          <br />
-          <slot name="link"></slot>
+    <div class="col-sm-6 col-md-4 col-lg-3">
+        <div id="root">
+            <div class="box">
+                <div class="content">
+                    <slot name="image"></slot>
+                    <slot name="title" class="title"></slot>
+                    <slot name="subtitle"></slot>
+                    <hr />
+                    <slot name="body" class="content"></slot>
+                    <br />
+                    <slot name="link"></slot>
+                    <br /><br />
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
 </template>
 
 <script>
