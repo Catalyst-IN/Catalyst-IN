2e9a59b974bf23a2dafd533e96fac996686faba8
Vinayak Sharma <54861487+vinayaksh42@users.noreply.github.com>
2020-06-17 04:07:39 +0530

    read more button

--- src/components/gridViewactive.vue
+++ src/components/gridViewactive.vue
@@ -1,46 +1,70 @@
 <template>
-  <div class="row">
-    <app-active v-for="project in info" :key="project.id">
-      <img
-        :src="require('../assets/' + project.imageLink)"
-        class="card-img-top"
-        slot="image"
-      />
-      <h5 slot="title">{{ project.title }}</h5>
-      <h6 slot="subtitle">{{ project.subtitle }}</h6>
-      <p slot="body">{{ project.body }}</p>
-      <button class="btn btn-info" slot="link">
-        <a :href="project.link" target="_blank">Know More</a>
-      </button>
-    </app-active>
-  </div>
+    <div class="row">
+        <app-active v-for="project in info" :key="project.id">
+            <img
+                :src="require('../assets/' + project.imageLink)"
+                class="card-img-top"
+                slot="image"
+                width="320"
+                height="190"
+            />
+            <h5 slot="title">{{ project.title }}</h5>
+            <h6 slot="subtitle">{{ project.subtitle }}</h6>
+
+            <div slot="body">
+                <span v-if="!project.readMoreActivated"
+                    >{{ project.body.slice(0, 50) }}
+                </span>
+                <a
+                    class=""
+                    v-if="!project.readMoreActivated"
+                    @click="
+                        project.readMoreActivated = !project.readMoreActivated
+                    "
+                    href="#/"
+                >
+                    Read more...
+                </a>
+                <span
+                    v-if="project.readMoreActivated"
+                    v-html="project.body"
+                ></span>
+            </div>
+
+            <!--      <p slot="body">{{ project.body }}</p>-->
+            <button class="btn btn-info" slot="link">
+                <a :href="project.link" target="_blank">Know More</a>
+            </button>
+        </app-active>
+    </div>
 </template>
 
 <script>
-import activeProjects from "./activeProjects";
+import activeProjects from './activeProjects';
 export default {
-  props: ["info"],
-  components: {
-    appActive: activeProjects,
-  },
+    name: 'Readmore',
+    props: ['info'],
+    components: {
+        appActive: activeProjects,
+    },
 };
 </script>
 
 <style scoped>
 .card-img-top {
-  padding-bottom: 20px;
+    padding-bottom: 20px;
 }
 h6 {
-  color: #8d2166;
+    color: #8d2166;
 }
 h5 {
-  color: #ec8e2a;
+    color: #ec8e2a;
 }
 button a {
-  color: white;
+    color: white;
 }
 
 button a:hover {
-  text-decoration: none;
+    text-decoration: none;
 }
 </style>
